054a1c3c307173089ed34c957cbcc8bc58b4bf07
WeiXinao <1632967698@qq.com>
2024-11-13 14:05:44 +0800

    test: schedualer

--- webook/internal/repository/dao/job.go
+++ webook/internal/repository/dao/job.go
@@ -28,18 +28,18 @@ func (dao *GORMJobDAO) Preempt(ctx context.Context, refreshInterval time.Duratio
 		now := time.Now().UnixMilli()
 		err := db.Model(&Job{}).Where("status = ? AND next_time < ?",
 			jobStatusWaiting, now).First(&j).Error
-		if err == gorm.ErrRecordNotFound {
-			err = db.Model(&Job{}).Where("status = ? AND utime < ?",
-				jobStatusRunning, time.Now().Add(-3*refreshInterval)).First(&j).Error
-			if err != nil {
-				return j, err
-			}
-		}
+		//if err == gorm.ErrRecordNotFound {
+		//	err = db.Model(&Job{}).Where("status = ? AND utime < ?",
+		//		jobStatusRunning, time.Now().Add(-3*refreshInterval)).First(&j).Error
+		//	if err != nil {
+		//		return j, err
+		//	}
+		//}
 		if err != nil {
 			return j, err
 		}
-		res := db.Model(&Job{}).Where("id != ? AND version = ?", j.Id, j.Version).
-			Where(map[string]any{
+		res := db.Model(&Job{}).Where("id = ? AND version = ?", j.Id, j.Version).
+			Updates(map[string]any{
 				"status":  jobStatusRunning,
 				"version": j.Version + 1,
 				"utime":   now,
@@ -58,7 +58,7 @@ func (dao *GORMJobDAO) Preempt(ctx context.Context, refreshInterval time.Duratio
 func (dao *GORMJobDAO) Release(ctx context.Context, id int64) error {
 	now := time.Now().UnixMilli()
 	return dao.db.WithContext(ctx).Model(&Job{}).
-		Where("id != ?", id).Updates(map[string]any{
+		Where("id = ?", id).Updates(map[string]any{
 		"status": jobStatusWaiting,
 		"utime":  now,
 	}).Error
@@ -66,14 +66,14 @@ func (dao *GORMJobDAO) Release(ctx context.Context, id int64) error {
 
 func (dao *GORMJobDAO) UpdateUtime(ctx context.Context, id int64) error {
 	now := time.Now().UnixMilli()
-	return dao.db.WithContext(ctx).Model(&Job{}).Where("id != ?", id).Updates(map[string]any{
+	return dao.db.WithContext(ctx).Model(&Job{}).Where("id = ?", id).Updates(map[string]any{
 		"utime": now,
 	}).Error
 }
 
 func (dao *GORMJobDAO) UpdateNextTime(ctx context.Context, id int64, t time.Time) error {
 	now := time.Now().UnixMilli()
-	return dao.db.WithContext(ctx).Model(&Job{}).Where("id != ?", id).Updates(map[string]any{
+	return dao.db.WithContext(ctx).Model(&Job{}).Where("id = ?", id).Updates(map[string]any{
 		"utime":     now,
 		"next_time": t.UnixMilli(),
 	}).Error
